4a7d03aa219409e9b0e1e2b6b74128ab0524fa62
Floris <Flomo123456789@gmail.com>
2020-03-02 16:54:22 +0100

    Somewhat improved lighting. All things that are wrong with it are posted on trello

---
+++ Assets/Script/Character/Player/Attack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bullet = null;
+
+    [SerializeField]
+    private GameObject currentWeapon;
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("Attack") > 0)
+        {
+            Instantiate(bullet, (Vector2)transform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Script/Item/Bullet/BulletMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    private Rigidbody2D mainRigid;
+
+    [SerializeField]
+    private float multiplier = 1;
+
+    void Start()
+    {
+        mainRigid = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        mainRigid.MovePosition((Vector2)transform.position + Vector2.right * multiplier * Time.deltaTime);
+    }
+}
