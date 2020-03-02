3f8e6519740208298fde89e37ff5e9e69f48fcf9
Floris <Flomo123456789@gmail.com>
2020-03-02 10:51:59 +0100

    Added Controlled_Movement

---
+++ Assets/Script/Character/Player/Controlled_Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlled_Movement : MonoBehaviour
+{
+    private Rigidbody2D mainRigid;
+
+    [SerializeField]
+    private float multiplier = 1;
+
+    void Start()
+    {
+        mainRigid = this.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 move = new Vector2();
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
+
+        mainRigid.MovePosition((Vector2)transform.position + move * multiplier * Time.deltaTime);
+    }
+}
