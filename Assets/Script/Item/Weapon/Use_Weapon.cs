4a9ff05dfc50e53ba01e0427b68136a8a36ae888
Floris <Flomo123456789@gmail.com>
2020-03-03 10:36:54 +0100

    More info for a weapon

--- Assets/Script/Item/Weapon/Use_Weapon.cs
+++ Assets/Script/Item/Weapon/Use_Weapon.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class Use_Weapon : MonoBehaviour
 {
     [SerializeField]
-    private bool isMelee, isAutomatic;
+    private bool isMelee, isFullyAutomatic;
+
+    [SerializeField]
+    private int fireRate;
+
+    // this is just a normal bullet now but i will make it changable depending on what mag is inserted after a while
+    [SerializeField]
+    private GameObject bullet = null;
 
     public void Use()
     {
-
+        if (Input.GetAxisRaw("Attack") > 0)
+        {
+            Instantiate(bullet, (Vector2)transform.position, Quaternion.identity);
+        }
     }
 }
