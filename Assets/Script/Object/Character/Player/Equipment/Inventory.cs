5751235d29c72609809e5e81584b3d5ac3ebba50
SirRolf <flomo123456789@gmail.com>
2020-03-11 16:13:27 +0100

    Added scriptable object for containers

--- Assets/Script/Object/Character/Player/Equipment/Inventory.cs
+++ Assets/Script/Object/Character/Player/Equipment/Inventory.cs
@@ -6,12 +6,36 @@ public class Inventory : MonoBehaviour
 {
     //avalable slots
     [SerializeField]
-    private GameObject backpackSlot = null;
+    private Container_Storage backpackSlot = null;
 
     private GameObject backpackSlotUI = null;
+
+    [SerializeField]
+    private GameObject containerItemPanel = null;
+    [SerializeField]
+    private GameObject slotUIElement = null;
+
+    private GameObject inventoryUI = null;
 
     void Start()
     {
         backpackSlotUI = GameObject.Find("BackpackSlot");
+        inventoryUI = GameObject.Find("InventoryUI");
+        Equip(backpackSlotUI, backpackSlot);
     }
+
+    public void Equip(GameObject slot, Container_Storage container)
+    {
+        GameObject _containerItemPanel = Instantiate(containerItemPanel, transform.position, Quaternion.identity);
+        _containerItemPanel.transform.SetParent(inventoryUI.transform);
+        _containerItemPanel.GetComponent<RectTransform>().sizeDelta = new Vector2(container.xSize * 100 + 20, container.ySize * 100 + 20);
+        _containerItemPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(300, -50);
+        int size = container.xSize * container.ySize;
+        for (int i = 0; i < size; i++)
+        {
+            GameObject _slotUIElement = Instantiate(slotUIElement, transform.position, Quaternion.identity);
+            _slotUIElement.transform.SetParent(_containerItemPanel.transform);
+        }
+    }
+
 }
--- Assets/Script/Object/Item/Container/Container_Storage.cs
+++ Assets/Script/Object/Item/Container/Container_Storage.cs
@@ -2,33 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Container_Storage : MonoBehaviour
+[CreateAssetMenu(fileName = "ContainerData", menuName = "Object/Container", order = 1)]
+public class Container_Storage : ScriptableObject
 {
-    [SerializeField]
-    private int xSize = 0;
-    [SerializeField]
-    private int ySize = 0;
-    private int size = 0;
+    public int xSize = 0;
+    public int ySize = 0;
 
-    [SerializeField]
-    private GameObject backpackItemPanel = null;
-    [SerializeField]
-    private GameObject slotUIElement = null;
-    private GameObject inventoryUI = null;
+    public GameObject[,] ItemData;
 
-    void Start()
-    {
-        size = xSize * ySize;
-        inventoryUI = GameObject.Find("InventoryUI");
-
-        GameObject _backpackItemPanel = Instantiate(backpackItemPanel, transform.position, Quaternion.identity);
-        _backpackItemPanel.transform.SetParent(inventoryUI.transform);
-        _backpackItemPanel.GetComponent<RectTransform>().sizeDelta = new Vector2(xSize * 100 + 20, ySize * 100 + 20);
-        _backpackItemPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(300, -50);
-        for (int i = 0; i < size; i++)
-        {
-            GameObject _slotUIElement = Instantiate(slotUIElement, transform.position, Quaternion.identity);
-            _slotUIElement.transform.SetParent(_backpackItemPanel.transform);
-        }
-    }
 }
