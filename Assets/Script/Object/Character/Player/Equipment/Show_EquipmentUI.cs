5013083d35d25ba08a6f8d9a9ca993e58af60ef6
SirRolf <flomo123456789@gmail.com>
2020-03-11 11:05:53 +0100

    Inventory screen can now be toggled off and on

---
+++ Assets/Script/Object/Character/Player/Equipment/Show_EquipmentUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Show_EquipmentUI : MonoBehaviour
+{
+    private GameObject inventoryUIParent = null;
+
+    void Start()
+    {
+        inventoryUIParent = GameObject.Find("InventoryUIParent");
+    }
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("OpenInventory") != 0)
+        {
+            inventoryUIParent.SetActive(true);
+        }
+        else
+        {
+            inventoryUIParent.SetActive(false);
+        }
+    }
+}
