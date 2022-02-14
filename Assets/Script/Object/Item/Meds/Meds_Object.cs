8edc1edf16dead99faec50f59023ff1680cc299b
Floris van den Berg <floris.vandeberg@beyondsports.nl>
2022-02-14 10:34:07 +0100

    Github changes and started from scratch

--- Assets/Script/Object/Item/Meds/Meds_Object.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewMeds", menuName = "Object/Item/Meds", order = 1)]
-public class Meds_Object : Object_Item
-{
-    public int healAmount = 0;
-}
